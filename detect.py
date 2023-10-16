59d9adb057575e7ce12163ccc8c878fb9e26d9bc
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-10-16 16:39:54 +0800

    feat: Basic Hands Coins Iteraction

--- detect.py
+++ detect.py
@@ -4,25 +4,28 @@ import mediapipe as mp
 
 
 # Define Coin_Detection Function
-def coin_detection(frame):
+def coin_detection(frame, draw=False):
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     gray = cv.medianBlur(gray, 5)
     circles = cv.HoughCircles(
-        gray,
-        cv.HOUGH_GRADIENT,
-        1,
-        100,
-        param1=80,
-        param2=30,
-        minRadius=0,
-        maxRadius=100,
+        image=gray,
+        method=cv.HOUGH_GRADIENT,
+        dp=1,
+        minDist=10,
     )
 
     if circles is not None:
         circles = np.uint16(np.around(circles))
-        return circles[0][0]
+        # Draw Circles
+        if draw:
+            for idx in circles[0, :]:
+                # Draw Outer Circle
+                cv.circle(frame, (idx[0], idx[1]), idx[2], (0, 255, 0), 2)
+                # Draw Center of Circle
+                cv.circle(frame, (idx[0], idx[1]), 2, (0, 0, 255), 3)
+        return circles[0, :], frame
     else:
-        return None
+        return None, frame
 
 
 # Define Hand_Detection Function
@@ -34,13 +37,13 @@ def hand_detection(frame, draw=False):
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5,
     )
+    results = hands.process(frame)
 
     if draw:
         mpDraw = mp.solutions.drawing_utils
 
         # Convert the BGR image to RGB before processing
         frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        results = hands.process(frame)
 
         # Draw the hand annotations on the image
         if results.multi_hand_landmarks:
@@ -51,4 +54,22 @@ def hand_detection(frame, draw=False):
         frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
 
     # Return the Position of the Hand, and the Frame
-    return hands, frame
+    return results, frame
+
+
+# Define Whether Hand is Touching Coin
+def isTouching(hands, coins):
+    if coins is None or hands is None:
+        return False
+    if hands.multi_hand_landmarks:
+        for handLandmarks in hands.multi_hand_landmarks:
+            for coin in coins:
+                for landmark in handLandmarks.landmark:
+                    if (
+                        landmark.x * 640 > coin[0] - coin[2]
+                        and landmark.x * 640 < coin[0] + coin[2]
+                        and landmark.y * 480 > coin[1] - coin[2]
+                        and landmark.y * 480 < coin[1] + coin[2]
+                    ):
+                        return True
+    return False
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 # Libraries
-import mediapipe as mp  # Import MediaPipe for Hand Detection
-import numpy as np  # Import NumPy for Array Manipulation
 import cv2 as cv  # Import OpenCV for Image Manipulation
 import sys  # Import System for Error Handling
 
 # Other Python Files
-from detect import hand_detection, coin_detection  # Import Hand Detection Function
+from detect import (
+    hand_detection,
+    coin_detection,
+    isTouching,
+)  # Import Detection Function
 from macros import fprint, waitUser  # Import Print Function
 
+
+mode = "SHOW"
+hover = False
+touch = 0
 
 # Define Absolute Path to Model
 modelPath = sys.path[0] + "hand_landmarker.task"
@@ -32,39 +38,66 @@ fprint("M", "Capturing Background...")
 # Save 1st Frame as Background
 ret, frame = cam.read()
 if not ret:  # if frame is read correctly ret is True
-    # eprint("Can't receive frame (stream end?). Exiting ...")
+    fprint("E", "Can't receive frame (stream end?). Exiting ...")
     exit()
 cv.imwrite("background.png", frame)
 fprint("M", "Background Captured!")
 
 # Wait for User to Press Space
 fprint("C", "<-- Press Space to Capture Coin Position -->")
-waitUser(" ", cam)
-fprint("M", "Capturing Coin Position...")
 
 # Save 2nd Frame with Coin Position
-ret, frame = cam.read()
-if not ret:  # if frame is read correctly ret is True
-    # eprint("Can't receive frame (stream end?). Exiting ...")
-    exit()
-coinPos = coin_detection(frame)
+while True:
+    ret, frame = cam.read()
+    if not ret:  # if frame is read correctly ret is True
+        fprint("E", "Can't receive frame (stream end?). Exiting ...")
+        exit()
+    coinPos, frame = coin_detection(frame, draw=True)  # Detect Coin
+    cv.imshow("frame", frame)  # Display frame
+    if cv.waitKey(1) == ord(" "):  # Handle Exit
+        break
+
+fprint("M", "Capturing Coin Position...")
 cv.imwrite("coin.png", frame)
 fprint("M", "Coin Position Captured!")
 
 
 # Handle While Camera is Open
 while True:
-    # Capture frame-by-frame
     ret, frame = cam.read()
     if not ret:  # if frame is read correctly ret is True
         fprint("E", "Can't receive frame (stream end?). Exiting ...")
         break
 
     # Detect Hand
-    hands, frame = hand_detection(frame, draw=True)
+    hands, frame = hand_detection(frame)
+
+    if isTouching(hands, coinPos) and not hover:
+        print("Touched!")
+        hover = True
+        touch += 1
+    elif not isTouching(hands, coinPos) and hover:
+        print("Leaved!")
+        hover = False
+
+    mode = "SHOW" if touch % 2 == 0 else "HIDE"
+
+    # Cover the Coin base on the Coin Position with the Background
+    background = cv.imread("background.png")
+    deviation = 20
+    if mode == "HIDE":
+        for circle in coinPos:
+            x, y, r = circle[0], circle[1], circle[2] + deviation
+            stX, stY, edX, edY = x - r, y - r, x + r, y + r
+            frame[stY:edY, stX:edX] = background[stY:edY, stX:edX]
 
     # Display the resulting frame
     cv.imshow("frame", frame)
+
+    # Manual Trigger Mode
+    if cv.waitKey(1) == ord(" "):
+        print("Triggered!")
+        touch += 1
 
     # Handle Exit
     if cv.waitKey(1) == ord("q"):
